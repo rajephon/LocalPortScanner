cc452eead30c91fce32432c736d1eca43dc3ddc5
Chanwoo Noh <rajephon@gmail.com>
2018-07-05 18:45:19 +0900

    added scan form ui

--- PortMapper/PortMapperViewController.swift
+++ PortMapper/PortMapperViewController.swift
@@ -13,6 +13,15 @@ class PortMapperViewController: NSViewController {
     let scanner = PortScannerWrapper()!
     
     @IBOutlet weak var lbOpenedPortList: NSTextField!
+    @IBOutlet weak var scanFormView: NSView!
+    
+    @IBOutlet weak var textPortScanStart: PortRangeField!
+    @IBOutlet weak var textPortScanEnd: PortRangeField!
+    @IBOutlet weak var labelErrorMessgae: NSTextField!
+    
+    func portScanStartWithRange(start:UInt16, end:UInt16) -> Void {
+        scanFormView.isHidden = true
+    }
     
     func portScanResultCallback(port:UInt16, state:PortState) -> Void {
         
@@ -21,8 +30,15 @@ class PortMapperViewController: NSViewController {
     func portScanFinish() -> Void {
         
     }
+    
+    func showErrorMessage(_ msg:String) -> Void {
+        labelErrorMessgae.isHidden = false
+        labelErrorMessgae.stringValue = "ERROR: " + msg
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         lbOpenedPortList.stringValue = ""
         
         scanner.setScanResultCallback({ (port, state) -> Void in
@@ -33,9 +49,7 @@ class PortMapperViewController: NSViewController {
             self.portScanFinish()
         })
         scanner.setMultiThread(true)
-        
-        
-        
+    
 //        var i: UInt16 = 1
 //        while i < 1024 {
 //            let result = scanner?.isOpen(i);
@@ -47,6 +61,26 @@ class PortMapperViewController: NSViewController {
 //        }
     }
     
+    @IBAction func clickScanBtn(_ sender: Any) {
+        guard let start = UInt16(textPortScanStart.stringValue),
+            let end = UInt16(textPortScanEnd.stringValue) else {
+                showErrorMessage("casting failed")
+                return
+        }
+        if start > end {
+            showErrorMessage("시작 범위는 마지막보다 작아야 합니다.")
+            return
+        }
+        if start <= 0 {
+            showErrorMessage("시작 범위는 0보다 커야 합니다.")
+            return
+        }
+        if end > 65535 {
+            showErrorMessage("마지막 범위는 65535보다 작아야 합니다.")
+            return
+        }
+        portScanStartWithRange(start: start, end: end)
+    }
 }
 
 extension PortMapperViewController {
---
+++ PortMapper/YKSLibrary.swift
@@ -0,0 +1,34 @@
+//
+//  YKSLibrary.swift
+//  PortMapper
+//
+//  Created by Chanwoo Noh on 2018. 7. 5..
+//  Copyright © 2018년 Chanwoo Noh. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+class NumberOnlyFormattter : NumberFormatter {
+    override func isPartialStringValid(_ partialString: String,
+                              newEditingString newString: AutoreleasingUnsafeMutablePointer<NSString?>?,
+                              errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?) -> Bool {
+        if partialString.isEmpty {
+            return true
+        }
+        if partialString.count > 5 {
+            return false
+        }
+        return UInt16(partialString) != nil
+    }
+}
+
+class PortRangeField : NSTextField {
+    override func awakeFromNib() {
+        formatter = NumberOnlyFormattter()
+        let color = NSColor(deviceRed: 0.784, green: 0.784, blue: 0.784, alpha: 1.0)
+        let font = NSFont.systemFont(ofSize: 10)
+        let attrs = [NSAttributedStringKey.foregroundColor: color, NSAttributedStringKey.font: font]
+        let placeHolderString = NSAttributedString(string: "1 ~ 65535", attributes: attrs)
+        placeholderAttributedString = placeHolderString
+    }
+}
