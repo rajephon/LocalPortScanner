d689f0cc71c48180a5f0f8cebf171fe00e778d39
Chanwoo Noh <rajephon@gmail.com>
2018-07-18 19:49:50 +0900

    added AppIcons, change Statusbar image

--- PortScanner/PreferenceViewController.swift
+++ PortScanner/PreferenceViewController.swift
@@ -13,9 +13,15 @@ class PreferenceViewController : NSViewController {
 
     @IBOutlet weak var autoLaunchCheckbox: NSButton!
     
+    var autoLaunchEnabled:Bool = false
+    let keyAutoLaunchEnabled:String = "LoginItemEnabled"
+    
     // MARK: - NSViewController
     override func viewDidLoad() {
         super.viewDidLoad()
+        autoLaunchEnabled = UserDefaults.standard.bool(forKey: keyAutoLaunchEnabled)
+        
+        autoLaunchCheckbox.state = autoLaunchEnabled ? NSControl.StateValue.on : NSControl.StateValue.off
     }
     
     override func viewWillAppear() {
@@ -25,13 +31,14 @@ class PreferenceViewController : NSViewController {
     @IBAction func set(sender: NSButton) {
         let appBundleIdentifier = "so.yoko.portscanner.LaunchHelper"
         let autoLaunch = (autoLaunchCheckbox.state == NSControl.StateValue.on)
+        
+        UserDefaults.standard.set(autoLaunch, forKey: keyAutoLaunchEnabled)
         if SMLoginItemSetEnabled(appBundleIdentifier as CFString, autoLaunch) {
             if autoLaunch {
                 NSLog("Successfully add login item.")
             } else {
                 NSLog("Successfully remove login item.")
             }
-
         } else {
             NSLog("Failed to add login item.")
         }
